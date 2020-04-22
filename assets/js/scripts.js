a3abd0210c94c889b890c0682b715967f9f2d513
DanieleDedo <danielededo@icloud.com>
2020-04-22 17:15:09 +0200

    breadcrumb events

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,19 @@
 var months=["Jan","Feb"];
 
-function myFunctionJan(el) {
-  var x = document.getElementById("Jan");
+function myFunction(el,str) {
+  var x = document.getElementById(str);
   var y = document.getElementsByClassName("month");
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < months.length; i++) {
     if(document.getElementById(months[i]).style.display==='block'){
       document.getElementById(months[i]).style.display='none';
-      y[i].style.textDecoration="none"
+      y[i].style.textDecoration="none";
     }
   }
   x.style.display='block';
   el.style.textDecoration = "underline";
+  document.getElementById("monthClick").textContent=el.textContent;
+  document.getElementById("monthClick").style.display='block';
+  document.getElementById("bread").style.display='block';
+  document.getElementById("crumb").style.display='none';
   console.log(el.textContent);
-}
-
-function myFunctionFeb(el) {
-  var x = document.getElementById("Feb");
-  var y = document.getElementsByClassName("month");
-  for (var i = 0; i < 2; i++) {
-    if(document.getElementById(months[i]).style.display==='block'){
-      document.getElementById(months[i]).style.display='none';
-      y[i].style.textDecoration="none"
-    }
-  }
-  x.style.display='block';
-  el.style.textDecoration = "underline";
 }

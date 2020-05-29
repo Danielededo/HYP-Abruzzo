7a9dd9028af1266d7acca4535aa2021b50146208
mariodiodoardo <mdo96@Pc-Mario.localdomain>
2020-05-29 17:41:33 +0200

    refactor directory and added onmouselistener

---
+++ www/assets/js/person.js
@@ -0,0 +1,75 @@
+function getperson(){
+  fetch("../v2/people/" + localStorage.getItem("Id_person"))
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error("HTTP error, status = " + response.status);
+    }
+    return response.json();
+  })
+  .then(function(json) {
+    var item0 = document.getElementById("NamePerson");
+    var item1 = document.createElement("b");
+    let { Name , Description , Image} = json[0];
+    document.title = `${Name}`;
+    var li = document.getElementById("pathName");
+    li.innerHTML = `${Name}`;
+    item1.textContent = `${Name}`;
+    item0.appendChild(item1);
+    var item2 = document.getElementById("profile");
+    item2.src = `${Image}`;
+    var item3 = document.getElementById("descript");
+    item3.textContent = `${Description}`;
+    fetch("../v2/event/eventPerson/" + localStorage.getItem("Id_person"))
+    .then(function(response) {
+        if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+    }
+      return response.json();
+    })
+    .then(function(even) {
+      var contact = document.getElementById("contactEvent");
+      if(even.length == 0){
+         contact.textContent = "No event managed.";
+      }else{
+        let { Name , Id_event , Date} = even[0];
+        contact.href = "../pages/event.html";
+        contact.textContent = `${Name}`;
+        var x = `${Date}`.substring(5,7);
+        var z = "setEvent("+`${Id_event}`+","+x+")";
+        contact.setAttribute("onclick",z);
+        contact.setAttribute('onmouseover',z);
+      }
+    });
+    fetch("../v2/services/servicesPerson/" + localStorage.getItem("Id_person"))
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+      }
+      return response.json();
+    })
+    .then(function(serv) {
+      involveServ = document.getElementById("involveServ");
+      for(var i=0; i<serv.length;i++){
+        var listIt = document.createElement("li")
+        var involve = document.createElement("a");
+        involveServ.appendChild(listIt);
+        listIt.appendChild(involve);
+        let { Name , Id_service} = serv[i];
+        involve.href = "../pages/service.html";
+        involve.textContent = `${Name}`;
+        var z = "setService("+`${Id_service}`+")";
+        involve.setAttribute("onclick",z);
+        involve.setAttribute('onmouseover',z);
+      }
+    });
+  });
+}
+
+function setEvent(x,y){
+  localStorage.setItem("Id_event",x);
+  localStorage.setItem("month",y);
+}
+
+function setService(x){
+  localStorage.setItem("Id_service",x);
+}
---
+++ www/assets/js/service.js
@@ -0,0 +1,60 @@
+function getservice(){
+  fetch("../v2/services/" + localStorage.getItem("Id_service"))
+  .then(function(response) {
+      if (!response.ok) {
+      throw new Error("HTTP error, status = " + response.status);
+    }
+    return response.json();
+  })
+  .then(function(json) {
+    let { Name , Presentation , Image , Photos , Id_service} = json[0];
+    document.title = `${Name}`;
+    var li = document.getElementById("pathName");
+    li.innerHTML = `${Name}`;
+    var item0 = document.getElementById("NameService");
+    var item1 = document.createElement("b");
+    item1.textContent = `${Name}`;
+    item0.appendChild(item1);
+    document.getElementById("imgservice").src = `${Image}`;
+    var item2 = document.getElementById("presentation");
+    var item3 = document.createElement("p");
+    item3.innerHTML = `${Presentation}`;
+    item2.appendChild(item3);
+    document.getElementById("first").src = json[0].Photos[0];
+    document.getElementById("second").src = json[0].Photos[1];
+    document.getElementById("third").src = json[0].Photos[2];
+    document.getElementById("fourth").src = json[0].Photos[3];
+    fetch("../v2/event/eventService/" + `${Id_service}`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+      }
+      return response.json();
+    })
+    .then(function(even) {
+      var presents = document.getElementById("presents");
+      if(even.length == 0){
+        presents.textContent = "No event has the service in.";
+      }else{
+        var a = document.createElement("a");
+        presents.innerHTML = "This service is presented in the ";
+        presents.appendChild(a);
+        let { Name , Id_event , Date} = even[0];
+        a.href = "../pages/event.html";
+        a.textContent = `${Name}`;
+        var x = `${Date}`.substring(5,7);
+        var z = "setEvent("+`${Id_event}`+","+x+")";
+        a.setAttribute("onclick",z);
+        a.setAttribute('onmouseover',z);
+        var here = document.createTextNode(" event.");
+        presents.appendChild(here);
+      }
+    });
+    localStorage.setItem("name",`${Name}`);
+  });
+}
+
+function setEvent(x,y){
+  localStorage.setItem("Id_event",x);
+  localStorage.setItem("month",y);
+}
